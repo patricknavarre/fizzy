5e3f60ce6d7fab16acea74a2da8064a68261a19b
Patrick Navarre <patrick.navarre@codeimmersives.com>
2020-09-24 13:59:00 -0400

    attempted fizzy

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-function fizzy(num) {
-
+function fizzy(num1) {
+  if (num1 % 3 === 0 && num1 % 5 !== 0) {
+    return 'Fizz'
+  } else if (num1 % 5 === 0 && num1 % 3 !== 0) 
+  return 'Buzz'
+   else (num1 % 5 === 0 && num1 % 3 !== 0) 
+  return num1 ;
+  else (num1 % 3 = 0 && num1 % 5 = 0) 
+  return 'FizzBuzz'
 }
 
-module.exports = {
+
+
+
+  module.exports = {
   fizzy,
 }
