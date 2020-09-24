dda1eb3c3d1abc7346bea15a70483301e561fd10
Patrick Navarre <patrick.navarre@codeimmersives.com>
2020-09-24 14:14:53 -0400

    finished test - after explanation

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 function fizzy(num1) {
-  if (num1 % 3 === 0 && num1 % 5 !== 0) {
-    return 'Fizz'
-  } else if (num1 % 5 === 0 && num1 % 3 !== 0) 
-  return 'Buzz'
-   else (num1 % 5 === 0 && num1 % 3 !== 0) 
-  return num1 ;
-  else (num1 % 3 = 0 && num1 % 5 = 0) 
-  return 'FizzBuzz'
+  if (num1 % 3 === 0 && num1 % 5 === 0) {
+    return 'FizzBuzz' ;
+  } else if(num1 % 3 === 0)
+  return 'Fizz'
+  if (num1 % 5 === 0 && num1 % 3 !== 0) {
+    return 'Buzz' ;
+  } else {
+    return num1;
+  }
+
 }
 
 
